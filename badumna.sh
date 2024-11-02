8f6f709b705585e44edfc42e3bcabe14d67a1bd5
austin_m <1austinm@protonmail.com>
2024-11-01 23:04:05 -0500

    change passcode

--- badumna.sh
+++ badumna.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-confirmed_code="1234"
+confirmed_code="NSBE50"
 confirmation_message="Congratulations.... you've cracked the code. The key for the lockbox can be found at XYZ_LOCATION"
 
 read -sp "Enter the pass code >> " input_code
 
 while [ "$input_code" != "$confirmed_code" ]
 do
+  echo ""
   read -sp "Thats not it... think >> " input_code
 done
 
