f956d6e6b9e30159c50684ffebb81c14a33a2a83
austin_m <1austinm@protonmail.com>
2024-11-01 23:40:26 -0500

    typewriter

--- badumna.sh
+++ badumna.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 confirmed_code="NSBE50"
-confirmation_message="Congratulations.... you've cracked the clues. You will find the final key attached to the soldering iron stand. It is taped underneath the silver tray. Use it to open the lockbox and escape. When you do, come and find me; As clearly you all are worthy of joining my campaign. We have work to do ahead."
+confirmation_message="Congratulations.... you've cracked the clues. The final key is attached to the soldering iron stand, taped underneath the silver tray; You must lift it up. Use it to open the lockbox and escape. When you do, come and find me in Nairobi if you wish to join my mission to take down those who wish to keep us under their thumb. The road ahead won't be easy, but clearly you all are capable. But I myself will determine if you are worthy."
 
 read -sp "Enter the pass code >> " input_code
 
@@ -12,4 +12,12 @@ do
 done
 
 echo ""
-echo -e $confirmation_message 
+# echo -e $confirmation_message 
+
+delay=0.05
+
+for ((i = 0; i < ${#confirmation_message}; i++)); do
+    echo -n "${confirmation_message:$i:1}"
+    sleep $delay
+done
+echo
